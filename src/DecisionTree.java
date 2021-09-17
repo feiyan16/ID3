8aa7c456926aa3e3a08ac5ef55fa66168fca5bc5
feiyan16 <feiyan.su16@gmail.com>
2021-09-16 19:00:19 -0500

    Work on JDK 1.8

--- src/DecisionTree.java
+++ src/DecisionTree.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class Node {
 	HashMap<String, List<Integer>> data = new HashMap<>();
-	
+
 	double 
 	H, // entropy
 	IG = -1, // information gain
 	tot, // # of rows
 	ct0 = 0, ct1 = 0, ct2 = 0; // partitioned count of classes
-	
+
 	String attribute = "";
-	
+
 	// which branch it came from and 
 	// if leaf node, what class is it
 	int branch = -1, clss = -1; 
-	
+
 	Node left, mid, right; // 0, 1, 2 child
-	
+
 	void init() {
 		this.tot = ct0 + ct1 + ct2;
 		this.H = entropy();
-		
+
 	}
-	
+
 	boolean isLeaf() {
 		if(H == 0)
 			return true;
@@ -30,10 +30,10 @@ class Node {
 	}
 
 	double entropy() {
-		
+
 		if(tot == 0) 
 			return 0;
-		
+
 		double zero = (ct0/tot) * (log2(ct0/tot));
 		double one = (ct1/tot) * (log2(ct1/tot));
 		double two = (ct2/tot) * (log2(ct2/tot));
@@ -48,24 +48,22 @@ class Node {
 	void print() {
 		System.out.printf("%s = %d : ", attribute, branch);
 		System.out.print(clss == -1 ? "\n" : clss + "\n");
-//		System.out.printf("IG: %.2f\n", IG);
-//		System.out.printf("0: %d, 1: %d, 2: %d\n", (int)ct0, (int)ct1, (int)ct2);
 	}
 }
 
 public class DecisionTree {
-	
+
 	Node root;
 	double currentIG = -1;
 	String[] attributes; // keeps order of attributes L - R
-	
+
 	public DecisionTree(Node root, String[] attributes) {
 		this.root = root;
 		this.attributes = attributes;
 		init();
 		populate(root);
 	}
-	
+
 	public void init() {
 		List<Integer> classVal = root.data.get("class");
 		for(Integer in : classVal) {
@@ -75,18 +73,18 @@ public class DecisionTree {
 		}
 		root.init();
 	}
-	
+
 	public void populate(Node current) {
-		
+
 		if(current.isLeaf()) {
 			int max = (int) Math.max(Math.max(current.ct0, current.ct1), current.ct2);
 			current.clss = current.ct0 == max ? 0 : current.ct1 == max ? 1 : 2;
 			return;
 		} 
-		
+
 		String attribute = chooseAttribute(current);
 		Node[] nodes = partition(attribute, current);
-			
+
 		if(currentIG > 0) {	
 			for(Node n : nodes) {
 				n.attribute = attribute;
@@ -95,7 +93,7 @@ public class DecisionTree {
 				else if (n.branch == 1) current.mid = n;
 				else current.right = n;
 			}
-			
+
 			populate(current.left);
 			populate(current.mid);
 			populate(current.right);
@@ -109,21 +107,22 @@ public class DecisionTree {
 				int max = (int) Math.max(Math.max(root.ct0, root.ct1), root.ct2);
 				current.clss = (root.ct0 == max ? 0 : root.ct1 == max ? 1 : 2);
 			}
+			
 		}
-		
+
 	}
-	
+
 	public String chooseAttribute(Node current) {
 		// maps IG to attribute
 		HashMap<Double, String> map = new HashMap<>();
 		double MAX_IG = 0;
-		
+
 		for(String key : attributes) {
 			if(key.compareTo("class") == 0 || key.compareTo(current.attribute) == 0) 
 				continue;
-			
+
 			Node[] nodes = partition(key, current);
-			
+
 			// sum up conditional entropies
 			double condH = 0;
 			for(Node n : nodes) 
@@ -131,8 +130,9 @@ public class DecisionTree {
 			// calculate IG
 			double IG = current.H - condH;
 			
+
 			MAX_IG = Math.max(MAX_IG, IG);
-			
+
 			// only put into map if it's a unique IG
 			// if it's repeated, so map alr contains IG don't put, 
 			// it'll end up replacing the attribute that's already there
@@ -143,39 +143,39 @@ public class DecisionTree {
 		currentIG = MAX_IG;
 		return map.get(MAX_IG);
 	}
-	
+
 	public Node[] partition(String attribute, Node current) {
 		// initialize 3 nodes for branch 0, 1, and 2
 		Node[] nodes = new Node[] { new Node(), new Node(), new Node() };
 		nodes[0].branch = 0;
 		nodes[1].branch = 1;
 		nodes[2].branch = 2;
-		
+
 		// values for the relevant attribute
 		List<Integer> attributeValues = current.data.get(attribute);
 		// class values for current data set
 		List<Integer> classValues = current.data.get("class");
-		
+
 		for(Node n : nodes) {
 			for(String key : current.data.keySet()) 
 				n.data.put(key, new ArrayList<>());
-			
+
 			for(int i = 0; i < attributeValues.size(); i++) {
 				int v = attributeValues.get(i); // attribute value;
 				int c = classValues.get(i); // corresponding class to value;
-				
+
 				if(v == n.branch) {
 					if(c == 0) n.ct0++;
 					else if (c == 1) n.ct1++;
 					else if (c == 2) n.ct2++;
-					
+
 					for(String key : n.data.keySet()) 
 						n.data.get(key).add(current.data.get(key).get(i));
 				}
 			}
 			n.init();
 		}
-		
+
 		return nodes;
 	}
 
@@ -186,13 +186,14 @@ public class DecisionTree {
 	public void traverse(Node current, int level) {
 		if(current == null)
 			return;
-		
+
 		if(level > 0) {
 			if (level > 1)
 				for(int i = 0; i < level-1; i++) 
 					System.out.print("| ");
 
 			current.print();
+//			System.out.println(current.isLeaf());
 		} 
 		traverse(current.left, level + 1);
 		traverse(current.mid, level + 1);
--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,21 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		
+		String trainingPath = null;
+		String testPath = null;
+		if(args.length < 2) {
+			System.out.println("Please enter training data and test data");
+			System.exit(0);
+		} else {
+			trainingPath = args[0];
+			testPath = args[1];
+		}
+		
 		// get path and create input stream
-		final String trainingPath = "/Users/feiyansu/Desktop/4375/HW1/train.dat";
-		final String testPath = "/Users/feiyansu/Desktop/4375/HW1/test.dat";
 		FileInputStream trainingFile = new FileInputStream(trainingPath);
 		FileInputStream testFile = new FileInputStream(testPath);
-		
+
 		// create root node for tree
 		Node root = new Node();
 		// read file into root node
@@ -19,55 +28,56 @@ public class Main {
 		// print tree
 		tree.print();
 		System.out.println();
-		
+
 		// read store data from files for testing
 		HashMap<String, List<Integer>> testData = new HashMap<>();
 		HashMap<String, List<Integer>> trainingData = new HashMap<>();
-		for(String key : root.data.keySet()) trainingData.put(key, root.data.get(key));
+		trainingFile = new FileInputStream(trainingPath);
+		readFile(trainingFile, trainingData);
 		readFile(testFile, testData);
-		
+
 		Test testTraining = new Test(tree, trainingData, "training");
 		testTraining.runTest();
-		
+
 		Test testTest = new Test(tree, testData, "test");
 		testTest.runTest();
-		
+
 	}
 
 	public static String[] readFile(FileInputStream file, HashMap<String, List<Integer>> data)
 			throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(file));
-		
+
 		String headStr = reader.readLine();
 		String[] headers = headStr.split("\t");
-		
+
 		// initializes empty arrays for each header's values
 		for(int i = 0; i < headers.length; i++) 
 			data.put(headers[i], new ArrayList<>());
-		
+
 		String line = reader.readLine();
 		while(line != null) {
-			
-			if(line.isBlank() || line.isEmpty()) {
+
+			if(line.isEmpty() || line.startsWith(" ") || line.startsWith("\t")) {
 				line = reader.readLine();
 				continue;
 			}
-			
+
 			String[] values = line.split("\t");
-			
+
 			for(int i = 0; i < headers.length; i++) {
-				
-				if(!(values[i].isBlank() && values[i].isEmpty())) {
+
+				if(!(values[i].startsWith(" ") && values[i].startsWith("\t") && values[i].isEmpty())) {
 					int value = Integer.valueOf(values[i]);
 					data.get(headers[i]).add(value);
 				}
 			}
-			
+
 			line = reader.readLine();
 		}
-		
+
 		reader.close();
-		
+
 		return headers;
 	}
 }
